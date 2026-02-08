f369d625814310ce1bf1a3229a9e62d3456aa142
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-08 17:03:37 -0500

    Create MessageDto.cs

---
+++ backend/AchievementTracker.Api/Models/DTOs/Steam/MessageDto.cs
@@ -0,0 +1,11 @@
+namespace AchievementTracker.Api.Models.DTOs.DirectMessages;
+
+public sealed class MessageDto
+{
+     public long DirectMessageId { get; set; }
+     public int ConversationId { get; set; }
+     public int SenderAppUserId { get; set; }
+     public string Content { get; set; } = string.Empty;
+     public DateTime SentDate { get; set; }
+     public DateTime? ReadDate { get; set; }
+}
