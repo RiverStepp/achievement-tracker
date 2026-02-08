43df0c65d7cbb784d0e95aa10762516b5a2a72d6
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-08 17:41:03 -0500

    Create ConversationConfiguration.cs

---
+++ backend/AchievementTracker.Data/Data/Configurations/ConversationConfiguration.cs
@@ -0,0 +1,27 @@
+using AchievementTracker.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AchievementTracker.Data.Data.Configurations;
+
+public sealed class ConversationConfiguration : IEntityTypeConfiguration<Conversation>
+{
+     public void Configure(EntityTypeBuilder<Conversation> b)
+     {
+          b.HasKey(x => x.ConversationId);
+
+          b.Property(x => x.IsActive).HasDefaultValue(true);
+          b.Property(x => x.CreateDate).HasDefaultValueSql("SYSUTCDATETIME()");
+          b.Property(x => x.UpdateDate).HasDefaultValueSql("SYSUTCDATETIME()");
+
+          b.HasMany(x => x.Participants)
+               .WithOne(x => x.Conversation)
+               .HasForeignKey(x => x.ConversationId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+          b.HasMany(x => x.Messages)
+               .WithOne(x => x.Conversation)
+               .HasForeignKey(x => x.ConversationId)
+               .OnDelete(DeleteBehavior.Cascade);
+     }
+}
