c38a263f7884c611acf0371b930379cdafd15639
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-18 02:04:50 -0500

    Create NotificationConfiguration.cs

---
+++ backend/AchievementTracker.Data/Data/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,30 @@
+using AchievementTracker.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AchievementTracker.Data.Data.Configurations;
+
+public sealed class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+     public void Configure(EntityTypeBuilder<Notification> b)
+     {
+          b.HasKey(x => x.NotificationId);
+
+          b.Property(x => x.NotificationType).IsRequired();
+          b.Property(x => x.ReferenceId).HasMaxLength(200);
+          b.Property(x => x.CreatedDate).HasDefaultValueSql("SYSUTCDATETIME()");
+
+          b.HasIndex(x => new { x.RecipientAppUserId, x.CreatedDate });
+          b.HasIndex(x => new { x.RecipientAppUserId, x.ReadDate });
+
+          b.HasOne(x => x.Recipient)
+               .WithMany()
+               .HasForeignKey(x => x.RecipientAppUserId)
+               .OnDelete(DeleteBehavior.NoAction);
+
+          b.HasOne(x => x.Actor)
+               .WithMany()
+               .HasForeignKey(x => x.ActorAppUserId)
+               .OnDelete(DeleteBehavior.NoAction);
+     }
+}
