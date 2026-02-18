2c82e68a6d2953f40906d67f96869ffb4ae70197
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-18 02:07:54 -0500

    Create Notification.cs

---
+++ backend/AchievementTracker.Data/Entities/Notification.cs
@@ -0,0 +1,19 @@
+using AchievementTracker.Data.Enums;
+
+namespace AchievementTracker.Data.Entities;
+
+public sealed class Notification
+{
+     public long NotificationId { get; set; }
+     public int RecipientAppUserId { get; set; }
+     public int ActorAppUserId { get; set; }
+     public eNotificationType NotificationType { get; set; }
+     public string? ReferenceId { get; set; }
+     public DateTime CreatedDate { get; set; }
+     public DateTime? ReadDate { get; set; }
+
+     #region navigation
+     public AppUser Recipient { get; set; } = null!;
+     public AppUser Actor { get; set; } = null!;
+     #endregion
+}
