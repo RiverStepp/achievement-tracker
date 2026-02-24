23a7c06727ec61107194544f09f2bf9b952cb0be
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-24 01:37:20 -0500

    Update ConversationParticipantConfiguration.cs
    added last read message

--- backend/AchievementTracker.Data/Data/Configurations/ConversationParticipantConfiguration.cs
+++ backend/AchievementTracker.Data/Data/Configurations/ConversationParticipantConfiguration.cs
@@ -11,12 +11,18 @@ public sealed class ConversationParticipantConfiguration : IEntityTypeConfigurat
           b.HasKey(x => x.ConversationParticipantId);
 
           b.Property(x => x.JoinedDate).HasDefaultValueSql("SYSUTCDATETIME()");
+          b.Property(x => x.IsMuted).HasDefaultValue(false);
 
           b.HasIndex(x => new { x.ConversationId, x.AppUserId }).IsUnique();
 
           b.HasOne(x => x.AppUser)
                .WithMany()
                .HasForeignKey(x => x.AppUserId)
+               .OnDelete(DeleteBehavior.NoAction);
+
+               b.HasOne(x => x.LastReadMessage)
+               .WithMany()
+               .HasForeignKey(x => x.LastReadMessageId)
                .OnDelete(DeleteBehavior.NoAction);
      }
 }
