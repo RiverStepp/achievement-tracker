df76df71b1ea4bde5c1e7a5dd8550daa0738ba33
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-24 00:57:19 -0500

    Update ConversationConfiguration.cs

--- backend/AchievementTracker.Data/Data/Configurations/ConversationConfiguration.cs
+++ backend/AchievementTracker.Data/Data/Configurations/ConversationConfiguration.cs
@@ -12,10 +12,19 @@ public sealed class ConversationConfiguration : IEntityTypeConfiguration<Convers
 
           b.Property(x => x.ConversationType).IsRequired();
           b.Property(x => x.ConversationTitle).HasMaxLength(200);
+          b.Property(x => x.ConversationImageUrl).HasMaxLength(500);
+          b.Property(x => x.LastMessageDate);
 
           b.Property(x => x.IsActive).HasDefaultValue(true);
           b.Property(x => x.CreateDate).HasDefaultValueSql("SYSUTCDATETIME()");
           b.Property(x => x.UpdateDate).HasDefaultValueSql("SYSUTCDATETIME()");
+
+          b.HasIndex(x => new { x.CreatedByAppUserId, x.LastMessageDate });
+
+          b.HasOne(x => x.CreatedBy)
+               .WithMany()
+               .HasForeignKey(x => x.CreatedByAppUserId)
+               .OnDelete(DeleteBehavior.NoAction);
 
           b.HasMany(x => x.Participants)
                .WithOne(x => x.Conversation)
