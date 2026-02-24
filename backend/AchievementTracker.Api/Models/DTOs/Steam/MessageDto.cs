4a1620a0f943ea27b187f7b6364fbda921978293
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-24 01:46:22 -0500

    Update MessageDto.cs

--- backend/AchievementTracker.Api/Models/DTOs/Steam/MessageDto.cs
+++ backend/AchievementTracker.Api/Models/DTOs/Steam/MessageDto.cs
@@ -7,5 +7,4 @@ public sealed class MessageDto
      public int SenderAppUserId { get; set; }
      public string Content { get; set; } = string.Empty;
      public DateTime SentDate { get; set; }
-     public DateTime? ReadDate { get; set; }
 }
