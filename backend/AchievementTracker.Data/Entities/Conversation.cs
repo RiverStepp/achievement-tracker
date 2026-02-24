fd945742247c2efd13631cb02c2f9ede84a53fea
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-24 00:59:44 -0500

    Update Conversation.cs
    added image url for groupchat picture, last message date, and who started the conversation

--- backend/AchievementTracker.Data/Entities/Conversation.cs
+++ backend/AchievementTracker.Data/Entities/Conversation.cs
@@ -8,8 +8,12 @@ public sealed class Conversation : AuditableEntity
      public int ConversationId { get; set; }
      public eConversationType ConversationType { get; set; }
      public string? ConversationTitle { get; set; }
+     public string? ConversationImageUrl { get; set; }
+     public int CreatedByAppUserId { get; set; }
+     public DateTime? LastMessageDate { get; set; }
 
      #region navigation
+     public AppUser CreatedBy { get; set; } = null!;
      public List<ConversationParticipant> Participants { get; set; } = [];
      public List<DirectMessage> Messages { get; set; } = [];
      #endregion
