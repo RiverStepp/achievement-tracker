5a0225faa90ee7c143b88b07a219991d3ae5407b
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-24 00:49:23 -0500

    Update Conversation.cs
    included fields for Conversationtype and Conversationtitle

--- backend/AchievementTracker.Data/Entities/Conversation.cs
+++ backend/AchievementTracker.Data/Entities/Conversation.cs
@@ -1,10 +1,13 @@
 using AchievementTracker.Data.Entities.Common;
+using AchievementTracker.Data.Enums;
 
 namespace AchievementTracker.Data.Entities;
 
 public sealed class Conversation : AuditableEntity
 {
      public int ConversationId { get; set; }
+     public eConversationType ConversationType { get; set; }
+     public string? ConversationTitle { get; set; }
 
      #region navigation
      public List<ConversationParticipant> Participants { get; set; } = [];
