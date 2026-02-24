8be5af9defbbe20fa4639892a104be1a9f63169c
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-02-24 01:28:23 -0500

    Update ConversationParticipant.cs
    each user has their own read cursor which should be supported in group DMs. Also added the muted conversations, leave date, and last read message

--- backend/AchievementTracker.Data/Entities/ConversationParticipant.cs
+++ backend/AchievementTracker.Data/Entities/ConversationParticipant.cs
@@ -8,9 +8,14 @@ public class ConversationParticipant : AuditableEntity
      public int ConversationId { get; set; }
      public int AppUserId { get; set; }
      public DateTime JoinedDate { get; set; }
+     public DateTime? LeftDate { get; set; }
+     public bool IsMuted { get; set; }
+     public long? LastReadMessageId { get; set; }
+
 
      #region navigation
      public Conversation Conversation { get; set; } = null!;
      public AppUser AppUser { get; set; } = null!;
+     public DirectMessage? LastReadMessage { get; set; }
      #endregion
 }
