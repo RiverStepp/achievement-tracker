7cca297ed8db6430a8be26734bffa354b2231e49
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-03-22 16:39:31 -0400

    Delete backend/AchievementTracker.Api/Models/DTOs/Leaderboard/AchievementSummaryDto.cs

--- backend/AchievementTracker.Api/Models/DTOs/Leaderboard/AchievementSummaryDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AchievementTracker.Api.Models.DTOs.Leaderboard;
- 
-public sealed record AchievementSummaryDto(
-     int TotalAchievementsUnlocked,
-     int TotalGamesTracked,
-     int PerfectGamesCount,
-     DateTime? LastSyncedDate
-);
