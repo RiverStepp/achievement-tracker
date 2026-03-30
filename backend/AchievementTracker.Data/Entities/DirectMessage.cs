3012d4e8bd94b2a7aa6ca9e39838029860fd932c
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-03-30 03:38:35 -0400

    Update DirectMessage.cs

--- backend/AchievementTracker.Data/Entities/DirectMessage.cs
+++ backend/AchievementTracker.Data/Entities/DirectMessage.cs
@@ -1,16 +1,19 @@
+using AchievementTracker.Data.Entities.Common;
+
 namespace AchievementTracker.Data.Entities;
 
-public sealed class DirectMessage
+public class DirectMessage : AuditableEntity
 {
      public long DirectMessageId { get; set; }
      public int ConversationId { get; set; }
      public int SenderAppUserId { get; set; }
-     public string Content { get; set; } = string.Empty;
+     public required string Content { get; set; }
      public DateTime SentDate { get; set; }
      public DateTime? ReadDate { get; set; }
 
      #region navigation
      public Conversation Conversation { get; set; } = null!;
      public AppUser Sender { get; set; } = null!;
+     public ICollection<MessageEmbed> Embeds { get; set; } = [];
      #endregion
 }
