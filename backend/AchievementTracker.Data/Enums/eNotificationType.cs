7f223b79d7d18b78d854a31bbef4cdabcfd5194c
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-03-30 03:42:12 -0400

    Update eNotificationType.cs

--- backend/AchievementTracker.Data/Enums/eNotificationType.cs
+++ backend/AchievementTracker.Data/Enums/eNotificationType.cs
@@ -4,4 +4,8 @@ public enum eNotificationType : short
 {
      DirectMessage = 1,
      Like = 2
+     // An admin-triggered event notification for a time-sensitive or unpredictable achievement (like annual in-game events)
+     EventAchievement = 3,
+     // A scheduled notification for a date-specific achievement (like an achievement only obtainable on a particular date)
+     ScheduledAchievement = 4
 }
