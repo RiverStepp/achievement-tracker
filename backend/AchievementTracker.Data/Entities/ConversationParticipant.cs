53528ce8165ea572360178568507a88511fc8087
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-03-30 03:38:00 -0400

    Update ConversationParticipant.cs

--- backend/AchievementTracker.Data/Entities/ConversationParticipant.cs
+++ backend/AchievementTracker.Data/Entities/ConversationParticipant.cs
@@ -1,14 +1,21 @@
+using AchievementTracker.Data.Entities.Common;
+
 namespace AchievementTracker.Data.Entities;
 
-public sealed class ConversationParticipant
+public class ConversationParticipant : AuditableEntity
 {
      public int ConversationParticipantId { get; set; }
-     public int ConversationId { get; set; }
+     public Guid AppUserPublicId { get; set; }
      public int AppUserId { get; set; }
      public DateTime JoinedDate { get; set; }
+     public DateTime? LeftDate { get; set; }
+     public bool IsMuted { get; set; }
+     public long? LastReadMessageId { get; set; }
+ 
 
      #region navigation
      public Conversation Conversation { get; set; } = null!;
      public AppUser AppUser { get; set; } = null!;
+     public DirectMessage? LastReadMessage { get; set; }
      #endregion
 }
