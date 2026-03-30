392bc7ad00814db25310b43f127be388ed6b4d5b
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-03-30 03:30:09 -0400

    Update ConversationConfiguration.cs
    reverted changes, for DM PR

--- backend/AchievementTracker.Data/Data/Configurations/ConversationConfiguration.cs
+++ backend/AchievementTracker.Data/Data/Configurations/ConversationConfiguration.cs
@@ -10,18 +10,30 @@ public sealed class ConversationConfiguration : IEntityTypeConfiguration<Convers
      {
           b.HasKey(x => x.ConversationId);
 
+          b.Property(x => x.ConversationType).IsRequired();
+          b.Property(x => x.ConversationTitle).HasMaxLength(200);
+          b.Property(x => x.ConversationImageUrl).HasMaxLength(500);
+          b.Property(x => x.LastMessageDate);
+          
           b.Property(x => x.IsActive).HasDefaultValue(true);
           b.Property(x => x.CreateDate).HasDefaultValueSql("SYSUTCDATETIME()");
           b.Property(x => x.UpdateDate).HasDefaultValueSql("SYSUTCDATETIME()");
 
+          b.HasIndex(x => new { x.CreatedByAppUserId, x.LastMessageDate });
+ 
+          b.HasOne(x => x.CreatedBy)
+               .WithMany()
+               .HasForeignKey(x => x.CreatedByAppUserId)
+               .OnDelete(DeleteBehavior.NoAction);
+
           b.HasMany(x => x.Participants)
                .WithOne(x => x.Conversation)
                .HasForeignKey(x => x.ConversationId)
-               .OnDelete(DeleteBehavior.Cascade);
+               .OnDelete(DeleteBehavior.NoAction);
 
           b.HasMany(x => x.Messages)
                .WithOne(x => x.Conversation)
                .HasForeignKey(x => x.ConversationId)
-               .OnDelete(DeleteBehavior.Cascade);
+               .OnDelete(DeleteBehavior.NoAction);
      }
 }
