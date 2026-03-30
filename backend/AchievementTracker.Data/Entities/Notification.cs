182ecbca066e29420a24374f243264ffaf7b557c
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-03-30 03:39:44 -0400

    Update Notification.cs

--- backend/AchievementTracker.Data/Entities/Notification.cs
+++ backend/AchievementTracker.Data/Entities/Notification.cs
@@ -1,15 +1,15 @@
+using AchievementTracker.Data.Entities.Common;
 using AchievementTracker.Data.Enums;
 
 namespace AchievementTracker.Data.Entities;
 
-public sealed class Notification
+public sealed class Notification : AuditableEntity
 {
      public long NotificationId { get; set; }
      public int RecipientAppUserId { get; set; }
      public int ActorAppUserId { get; set; }
      public eNotificationType NotificationType { get; set; }
      public string? ReferenceId { get; set; }
-     public DateTime CreatedDate { get; set; }
      public DateTime? ReadDate { get; set; }
 
      #region navigation
