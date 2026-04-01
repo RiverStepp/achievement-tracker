4f6cd966fdce8bb99b1fc830ba39d009893a204d
StartingEmber <102005875+StartingEmber@users.noreply.github.com>
2026-04-01 01:05:33 -0400

    Update IAppUserRepository.cs
    tweaked to include the guid

--- backend/AchievementTracker.Api/DataAccess/Interfaces/IAppUserRepository.cs
+++ backend/AchievementTracker.Api/DataAccess/Interfaces/IAppUserRepository.cs
@@ -4,13 +4,13 @@ namespace AchievementTracker.Api.DataAccess.Interfaces;
 
 public interface IAppUserRepository
 {
-     Task<int> UpsertSteamUserAsync(
+     Task<(int AppUserId, Guid PublicId)> UpsertSteamUserAsync(
           long steamId64,
           string canonicalSteamId,
           SteamProfileDto? steamProfile,
           CancellationToken ct = default
      );
-     Task<int?> GetAppUserIdBySteamIdAsync(string canonicalSteamId, CancellationToken ct = default);
+     Task<(int AppUserId, Guid PublicId)?> GetAppUserBySteamIdAsync(string canonicalSteamId, CancellationToken ct = default);
      // note to self: other methods don't need to be in the interface because they are just "implementation details"
      // helps avoid clutter
 }
